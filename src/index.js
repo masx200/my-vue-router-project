e1f596734c13800abb2701ae5f8838cd49adea8e
masx200 <34191203+masx200@users.noreply.github.com>
2019-07-22 22:24:12 +0800

    修复clipboard的bug

--- src/index.js
+++ src/index.js
@@ -7,13 +7,17 @@ document.getElementById("root").innerHTML = `<div>
 import("./vue-index-render.js");
 import "./index.css";
 // eslint-disable-next-line no-undef
-import("bootstrap/dist/js/bootstrap.bundle");
-import("./clipboard.min.js").then(module => {
-  const ClipboardJS = module.default;
-  new ClipboardJS(".btn").on("success", function(e) {
-    e.clearSelection();
+
+window.addEventListener("load", () => {
+  import("bootstrap/dist/js/bootstrap.bundle");
+  import("./clipboard").then(module => {
+    const ClipboardJS = module.default;
+    new ClipboardJS(".btn").on("success", function(e) {
+      e.clearSelection();
+    });
   });
 });
+
 import "webpack-react-vue-spa-awesome-config/registerserviceworker.js";
 //import "./registerserviceworker";
 // (() => {
