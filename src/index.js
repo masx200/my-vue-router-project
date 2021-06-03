49f5e3ee69c404adf867845662ded1e5d8e9b9cf
masx200 <masx200@qq.com>
2021-06-03 11:13:14 +0800

    升级

--- src/index.js
+++ src/index.js
@@ -1,61 +1,58 @@
-import "./error-alert.js";
-import VueRouter from "vue-router";
-import SimpleStoreManager from "@masx200/vue-simple-global-state-store-manager"; // bindGlobalStore // initGlobalState,
-import Vue from "vue";
-import "@masx200/webpack-react-vue-spa-awesome-config/registerserviceworker.js";
 import "./index.css";
-import "./juejin-highlight-markdown-0.c4305b077afc652cb76a.css";
-import "./mystyle.css";
-import "./pre-wrap.css";
-// import "webpack-react-vue-spa-awesome-config/ie11babelpolyfill";
-("use strict");
-document.getElementById("root").innerHTML = `<div>
-<h1>loading</h1>
-<span class="mui-spinner mui-spinner-custom">
+alert("hello world");
+/*import "../registerserviceworker.js";
 
-</span></div>`;
+ import React from "react";
+import { render } from "react-dom";
 
-// eslint-disable-next-line no-undef
+function App() {
+  return <div>hello world</div>;
+}
 
-window.addEventListener(
-    "load",
-    () => {
-        //   import("bootstrap/dist/js/bootstrap.bundle");
-        import("clipboard").then((module) => {
-            const ClipboardJS = module.default;
-            new ClipboardJS(".btn").on("success", function (e) {
-                e.clearSelection();
-            });
-        });
-    },
-    { once: true }
-);
+render(<App />, document.getElementById("root"));
+ 
 
-Vue.config.productionTip = false;
-Vue.config.silent = true;
-Vue.config.devtools = true;
-Vue.use(VueRouter);
-// console.log(SimpleStore);
-Vue.use(SimpleStoreManager);
-Vue.config.errorHandler = function (err, vm, info) {
-    throw err;
-};
-import("./vue-index-render.js").then(({ router, default: AppHome }) => {
-    // var AppHome=default
-    new Vue({
-        el: document
-            .querySelector("#root")
-            .appendChild(document.createElement("div")),
-        router,
-        //   components: {
-        //     //   App
-        //   },
-        // template: "<App/>"
-        render(h) {
-            return h(AppHome);
-        },
-        // data() {
-        //   return {};
-        // }
-    });
+import Vue from "vue/dist/vue.esm.browser";
+const App = Vue.extend({
+  template: `
+    <div>
+    {{msg}}
+    </div>
+    
+    `,
+
+  name: "App",
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  },
+  methods: {},
+  mounted() {}
 });
+
+new Vue({
+  el: "#root",
+  // router,
+  components: {
+    App
+  },
+  template: "<App/>",
+  data() {
+    return {};
+  }
+});
+ 
+var msg="htmlWelcome to Your Vue.js App"
+ function h(type, props, ...children) {
+  return { type, props, children };
+}
+console.log(html`
+  <a href="/">Hello!</a>
+  <App />
+  <div>hello world</div>
+  <div>
+    {{msg}}
+  </div>
+`);
+ */
---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import "./error-alert.js";
+import VueRouter from "vue-router";
+//@ts-ignore
+import SimpleStoreManager from "@masx200/vue-simple-global-state-store-manager"; // bindGlobalStore // initGlobalState,
+import Vue from "vue";
+import "@masx200/webpack-react-vue-spa-awesome-config/registerserviceworker.js";
+import "./index.css";
+import "./juejin-highlight-markdown-0.c4305b077afc652cb76a.css";
+import "./mystyle.css";
+import "./pre-wrap.css";
+// import "webpack-react-vue-spa-awesome-config/ie11babelpolyfill";
+("use strict");
+const rootele = document.getElementById("root");
+rootele &&
+    (rootele.innerHTML = `<div>
+<h1>loading</h1>
+<span class="mui-spinner mui-spinner-custom">
+
+</span></div>`);
+
+// eslint-disable-next-line no-undef
+
+window.addEventListener(
+    "load",
+    () => {
+        //   import("bootstrap/dist/js/bootstrap.bundle");
+        import("clipboard").then((module) => {
+            const ClipboardJS = module.default;
+            new ClipboardJS(".btn").on("success", function (e) {
+                e.clearSelection();
+            });
+        });
+    },
+    { once: true }
+);
+
+Vue.config.productionTip = false;
+Vue.config.silent = true;
+Vue.config.devtools = true;
+Vue.use(VueRouter);
+// console.log(SimpleStore);
+Vue.use(SimpleStoreManager);
+Vue.config.errorHandler = function (err, vm, info) {
+    throw err;
+};
+import("./vue-index-render.js").then(({ router, default: AppHome }) => {
+    // var AppHome=default
+    const vm = new Vue({
+        // el: document
+        //     .querySelector("#root")
+        //     .appendChild(document.createElement("div")),
+        router,
+        //   components: {
+        //     //   App
+        //   },
+        // template: "<App/>"
+        render(h) {
+            return h(AppHome);
+        },
+        // data() {
+        //   return {};
+        // }
+    });
+    rootele && vm.$mount(rootele.appendChild(document.createElement("div")));
+});
