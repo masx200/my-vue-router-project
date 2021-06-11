36d20bff27707a9863cdb2d682388642530efa26
masx200 <masx200@qq.com>
2021-06-11 17:21:25 +0800

    Update prettiercode.worker.js

--- src/prettiercode.worker.js
+++ src/prettiercode.worker.js
@@ -1,5 +1,6 @@
 import prettier from "prettier/esm/standalone.mjs";
 import parserbabel from "prettier/esm/parser-babel.mjs";
+import prettierconfig from "./prettierconfig.json";
 self.onmessage = (e) => {
     const source = e.data;
     const code = transform(source);
@@ -8,9 +9,15 @@ self.onmessage = (e) => {
 };
 function transform(code) {
     console.log(code);
-    var output = prettier.format(code, {
-        parser: "babel-ts",
-        plugins: [parserbabel],
-    });
+    var output = prettier.format(
+        code,
+        Object.assign(
+            {
+                parser: "babel-ts",
+                plugins: [parserbabel],
+            },
+            prettierconfig
+        )
+    );
     return output;
 }
