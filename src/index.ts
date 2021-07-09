13b5b6e8f1a012435304843a7357146aa8b82715
masx200 <masx200@qq.com>
2021-07-09 15:58:38 +0800

    把css加回来

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "./styles.ts";
 //@ts-ignore
 import { initloadingid } from "./initloadingid.ts";
 import "./error-alert.js";
@@ -6,10 +7,7 @@ import VueRouter from "vue-router";
 // import SimpleStoreManager from "@masx200/vue-simple-global-state-store-manager"; // bindGlobalStore // initGlobalState,
 import Vue from "vue";
 import "@masx200/webpack-react-vue-spa-awesome-config/registerserviceworker.js";
-import "./index.css";
-// import "./juejin-highlight-markdown-0.c4305b077afc652cb76a.css";
-import "./mystyle.css";
-import "./pre-wrap.css";
+
 // import "webpack-react-vue-spa-awesome-config/ie11babelpolyfill";
 ("use strict");
 const rootele =
---
+++ src/styles.ts
@@ -0,0 +1,4 @@
+import "./index.css";
+import "./juejin-highlight-markdown-0.c4305b077afc652cb76a.css";
+import "./mystyle.css";
+import "./pre-wrap.css";
