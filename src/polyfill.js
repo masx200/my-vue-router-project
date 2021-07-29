6f3dc1aac1638b1c730eaa054b308181c92e1355
masx200 <masx200@qq.com>
2021-07-29 12:11:40 +0800

    删除proxypolyfill

--- src/polyfill.js
+++
@@ -1,7 +0,0 @@
-import importScripts from "./importscripts";
-
-if (typeof Proxy !== "function") {
-    importScripts(
-        "https://cdn.jsdelivr.net/npm/es6-proxy-polyfill@2.1.1/dist/es6-proxy-polyfill.min.js"
-    );
-}
