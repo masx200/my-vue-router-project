c9941e8b41651d235011474c869ad2cb5291fb65
masx200 <34191203+masx200@users.noreply.github.com>
2021-08-17 17:35:08 +0800

    格式化

--- src/jsfuck-and-hieroglyphy-decoder-and-encoder/hieroglyphy-encoder.vue
+++ src/jsfuck-and-hieroglyphy-decoder-and-encoder/hieroglyphy-encoder.vue
@@ -1,44 +1,40 @@
-<template>  <div class="container">
-            <h1 style="font-size: 30px">
-                编码hieroglyphy <br />encode hieroglyphy
-            </h1>
-            <p>
-                hieroglyphy是一个工具和javascript库，用于将字符串，数字和脚本转换为（）[]
-                {} +的等效序列！在浏览器中运行的字符。
-            </p>
-            <p>()+[]!{}</p>
-            <p>
-                浏览器兼容性:
-                建议在最新超高版本chrome或者Firefox或者safari浏览器中运行！
-            </p>
-            <br />
-            <textarea id="input" type="text"> </textarea>
-            <br />
-            <p>
-                <button
-                    class="btn btn-outline-success"
-                    id="encodescript"
-                    type="text"
-                >
-                    Encode script
-                </button>
+<template>
+    <div class="container">
+        <h1 style="font-size: 30px">
+            编码hieroglyphy <br />encode hieroglyphy
+        </h1>
+        <p>
+            hieroglyphy是一个工具和javascript库，用于将字符串，数字和脚本转换为（）[]
+            {} +的等效序列！在浏览器中运行的字符。
+        </p>
+        <p>()+[]!{}</p>
+        <p>
+            浏览器兼容性:
+            建议在最新超高版本chrome或者Firefox或者safari浏览器中运行！
+        </p>
+        <br />
+        <textarea id="input" type="text"> </textarea>
+        <br />
+        <p>
+            <button
+                class="btn btn-outline-success"
+                id="encodescript"
+                type="text"
+            >
+                Encode script
+            </button>
 
-                <button
-                    id="encodestring"
-                    type="text"
-                    class="btn btn-outline-info"
-                >
-                    Encode string
-                </button>
-            </p>
-            <br />
-            <br />
-            <textarea id="clip5869EC85-6180-4E4B-E565-561BA1F25223"> </textarea>
-            <div class="actions">
-                <span id="stats">0 chars</span>
-                <button class="btn btn-outline-primary" id="run">
-                    Run This
-                </button> <button
+            <button id="encodestring" type="text" class="btn btn-outline-info">
+                Encode string
+            </button>
+        </p>
+        <br />
+        <br />
+        <textarea id="clip5869EC85-6180-4E4B-E565-561BA1F25223"> </textarea>
+        <div class="actions">
+            <span id="stats">0 chars</span>
+            <button class="btn btn-outline-primary" id="run">Run This</button>
+            <button
                 class="
                     btn btn-outline-primary
                     mui-btn mui-btn-primary mui-btn-outlined
@@ -48,25 +44,29 @@
             >
                 复制
             </button>
-            </div>
-            <br />
-            <p>
-                <a
-                    href="https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER"
-                    >https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER</a
-                >
-            </p>
-            <p>
-                <a
-                    href="https://github.com/alcuadrado/hieroglyphy/blob/master/hieroglyphy.js"
-                    >https://github.com/alcuadrado/hieroglyphy/blob/master/hieroglyphy.js</a
-                >
-            </p>
-        </div></template>
+        </div>
+        <br />
+        <p>
+            <a
+                href="https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER"
+                >https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER</a
+            >
+        </p>
+        <p>
+            <a
+                href="https://github.com/alcuadrado/hieroglyphy/blob/master/hieroglyphy.js"
+                >https://github.com/alcuadrado/hieroglyphy/blob/master/hieroglyphy.js</a
+            >
+        </p>
+    </div>
+</template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
-export default defineComponent({mounted(){document.title='hieroglyphy encoder'},
+export default defineComponent({
+    mounted() {
+        document.title = "hieroglyphy encoder";
+    },
     setup() {},
 });
 </script>
--- src/jsfuck-and-hieroglyphy-decoder-and-encoder/jsfuck-encoder.vue
+++ src/jsfuck-and-hieroglyphy-decoder-and-encoder/jsfuck-encoder.vue
@@ -1,39 +1,39 @@
-<template>  <div class="container">
-            <h1 style="font-size: 30px">编码JSFUCK <br />encode JSFUCK</h1>
-            <p>
-                JSFuck是一种基于JavaScript原子部分的深奥教育编程风格。它只使用六个不同的字符来编写和执行代码。
-            </p>
-            <p>()+[]!</p>
-            <p>
-                它不依赖于浏览器，因此您甚至可以在Node.js上运行它。
-                <br />使用下面的表单转换您自己的脚本。取消选中“eval
-                source”以获取纯字符串。
-            </p>
-            <p>
-                浏览器兼容性:建议在最新超高版本的chrome或者Firefox或者safari浏览器中运行！
-            </p>
-            <br />
-            <textarea id="input" type="text"> </textarea>
-            <br />
-            <p>
-                <button class="btn btn-outline-primary" id="encode" type="text">
-                    Encode string
-                </button>
-                <button
-                    class="btn btn-outline-primary"
-                    id="encodescript"
-                    type="text"
-                >
-                    Encode script
-                </button>
-            </p>
+<template>
+    <div class="container">
+        <h1 style="font-size: 30px">编码JSFUCK <br />encode JSFUCK</h1>
+        <p>
+            JSFuck是一种基于JavaScript原子部分的深奥教育编程风格。它只使用六个不同的字符来编写和执行代码。
+        </p>
+        <p>()+[]!</p>
+        <p>
+            它不依赖于浏览器，因此您甚至可以在Node.js上运行它。
+            <br />使用下面的表单转换您自己的脚本。取消选中“eval
+            source”以获取纯字符串。
+        </p>
+        <p>
+            浏览器兼容性:建议在最新超高版本的chrome或者Firefox或者safari浏览器中运行！
+        </p>
+        <br />
+        <textarea id="input" type="text"> </textarea>
+        <br />
+        <p>
+            <button class="btn btn-outline-primary" id="encode" type="text">
+                Encode string
+            </button>
+            <button
+                class="btn btn-outline-primary"
+                id="encodescript"
+                type="text"
+            >
+                Encode script
+            </button>
+        </p>
 
-            <textarea id="clip9F02A223-F000-D1D5-5679-79F6719E9658"> </textarea>
-            <div class="actions">
-                <span id="stats">0 chars</span>
-                <button class="btn btn-outline-success" id="run">
-                    Run This
-                </button> <button
+        <textarea id="clip9F02A223-F000-D1D5-5679-79F6719E9658"> </textarea>
+        <div class="actions">
+            <span id="stats">0 chars</span>
+            <button class="btn btn-outline-success" id="run">Run This</button>
+            <button
                 class="
                     btn btn-outline-primary
                     mui-btn mui-btn-primary mui-btn-outlined
@@ -43,20 +43,21 @@
             >
                 复制
             </button>
-            </div>
-            <br />
-            <p>
-                <a
-                    href="https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER"
-                    >https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER</a
-                >
-            </p>
-            <p>
-                <a href="https://github.com/aemkei/jsfuck/blob/master/jsfuck.js"
-                    >https://github.com/aemkei/jsfuck/blob/master/jsfuck.js</a
-                >
-            </p>
-        </div></template>
+        </div>
+        <br />
+        <p>
+            <a
+                href="https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER"
+                >https://github.com/masx200/JSfuck-and-hieroglyphy-Decoder-and-ENCODER</a
+            >
+        </p>
+        <p>
+            <a href="https://github.com/aemkei/jsfuck/blob/master/jsfuck.js"
+                >https://github.com/aemkei/jsfuck/blob/master/jsfuck.js</a
+            >
+        </p>
+    </div>
+</template>
 <script lang="ts">
 import { defineComponent } from "vue";
 
