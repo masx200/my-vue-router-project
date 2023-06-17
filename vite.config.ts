72dc40c9cd64cd340dc054c4a32aebb9f9fd5c36
masx200 <34191203+masx200@users.noreply.github.com>
2023-06-17 12:30:28 +0800

    "@babel/plugin-proposal-private-property-in-object": "^7.21.11",

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
+    worker: { format: "es" },
     resolve: {
         alias: {
             "@": path.join(__dirname, "src/"),
