90109da047804dfbcefcc4b224585786d8d9949f
masx200 <masx200@qq.com>
2025-04-25 10:04:37 +0800

    build(deps): 更新 prettier 并添加 estree 插件
    - 将 prettier 版本从 3.0.3 升级到 3.5.3
    - 在 prettier 插件中添加 estree 支持
    - 优化 prettiercode-worker.js 中的错误处理和代码结构

--- src/beautify-javascript/prettiercode-worker.js
+++ src/beautify-javascript/prettiercode-worker.js
@@ -5,16 +5,27 @@
 import parserbabel from "@/assetsutils/prettier/parser-babel.js";
 import prettier from "@/assetsutils/prettier/standalone.js";
 import prettierconfig from "./prettierconfig.json";
-const prettierPlugins = [parserbabel];
+import prettierestree from "prettier/plugins/estree.mjs";
+
+const prettierPlugins = [parserbabel, prettierestree];
+
+self.addEventListener("unhandledrejection", fn2);
+function fn2(e) {
+    console.error("unhandledrejection", e);
+
+    throw e.reason;
+}
 //console.log(prettier, prettierPlugins);
-self.onmessage = (e) => {
+self.onmessage = async (e) => {
     const source = e.data;
-    const code = transform(source);
+    const code = await transform(source);
+
+    console.log({ code });
     // @ts-ignore
     self.postMessage(code);
 };
-function transform(code) {
-    console.log(code);
+async function transform(code) {
+    console.log({ code });
     var output = prettier.format(
         code,
         Object.assign(
